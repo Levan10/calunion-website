b080db409da6e30df5b77ff00ea504424055394f
Levan10 <evan.lerman.473@my.csun.edu>
2025-07-24 12:32:12 -0700

    Test

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById("contactBtn").addEventListener("click", () => {
+  window.location.href = "https://www.calunionfunding.com/default/applications/register?app_type=full";
+});
